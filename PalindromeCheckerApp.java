bde1f5d1a51015de056ff8d11b099635d3395857
vishwajitha21ccc <va4070@srmist.edu.in>
2026-02-27 15:27:23 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+package PalindromeChecker;
+public class PalindromeCheckerApp {
+    public static void main(String [] args){
+        System.out.println("Welcome to palindrome checker");
+        System.out.println("version 1.0");
+        System.out.println("system initialized successfully");
+
+    }
+    
+}
